85d5710dc983c614d7746df2b22388a1ba30e54a
Scott Dauer <swdauer@gmail.com>
2021-09-13 11:17:25 -0500

    Add shuffleMtoN

--- cards.c
+++ cards.c
@@ -10,14 +10,24 @@ void initDeck(deck d) {
 }
 
 /* Implementation of Fisher-Yates shuffle using Durstenfeld's Algorithm 235. */
+void shuffleMtoN(deck d, char m, char n) {
+    for (; n > m; n--) {
+        int j = rand() % (n - m + 1);
+        char holder = d[n];
+        d[n] = d[m + j];
+        d[m + j] = holder;
+    }
+}
+
 void shuffle(deck d) {
-    int i;
+    /* int i;
     for (i = DECK_SIZE - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         char holder = d[i];
         d[i] = d[j];
         d[j] = holder;
-    }
+    } */
+    shuffleMtoN(d, 0, DECK_SIZE - 1);
 }
 
 void printMtoN(deck d, char m, char n) {
