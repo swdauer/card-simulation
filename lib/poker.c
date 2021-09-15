1df8728970e5788ed052c8e448c537a0fee83e0a
Scott Dauer <swdauer@gmail.com>
2021-09-15 15:33:57 -0500

    Add countBits

--- lib/poker.c
+++ lib/poker.c
@@ -1,25 +1,52 @@
 #include "cards.h"
 #include "poker.h"
 
-handEvaluation evaluate(hand* h) {
-    handEvaluation flush;
-    handEvaluation straight;
-    handEvaluation straightFlush;
-    handEvaluation sets;
-
-    checkFlush(&flush, h);
-    checkStraight(&straight, h);
-
-    if (flush.handType && straight.handType) {
-        checkStraightFlush(&straightFlush, h);
-        if (straightFlush.handType) return straightFlush;
+unsigned char countBits(cardSet s) {
+    unsigned char count = 0;
+    for (; s != 0; s >>= 1) {
+        if (s & 0x1) count++;
     }
+    return count;
+}
 
-    checkSets(&sets, h);
+// handEvaluation evaluate(hand* h) {
+//     handEvaluation flush = {};
+//     handEvaluation straight = {};
+//     handEvaluation straightFlush = {};
+//     handEvaluation sets = {};
 
-    if (sets.handType == FOUR_OF_A_KIND) return sets;
-    else if (sets.handType == FULL_HOUSE) return sets;
-    else if (flush.handType == FLUSH) return flush;
-    else if (straight.handType == STRAIGHT) return straight;
-    else return sets;
+//     checkFlush(&flush, h);
+//     checkStraight(&straight, h);
+
+//     if (flush.handType && straight.handType) {
+//         checkStraightFlush(&straightFlush, h);
+//         if (straightFlush.handType) return straightFlush;
+//     }
+
+//     checkSets(&sets, h);
+
+//     if (sets.handType == FOUR_OF_A_KIND) return sets;
+//     else if (sets.handType == FULL_HOUSE) return sets;
+//     else if (flush.handType == FLUSH) return flush;
+//     else if (straight.handType == STRAIGHT) return straight;
+//     else return sets;
+// }
+
+// checkFlush returns after finding the first flush.
+// If the hand size is less than 10, then this will always be the best flush.
+// Otherwise there could be a better flush.
+void checkFlush(handEvaluation* e, hand* h) {
+    int i;
+    for (i = 0; i < NUM_SUITS; i++) {
+        if (countBits(h->bySuit[i]) >= 5) {
+            e->handType = FLUSH;
+            int j, count = 0;
+            for (j = NUM_RANKS - 1; j > 0 && count < 5; j--) {
+                if ((h->bySuit[i]) & (0x1 << j)) {
+                    e->h.byRank[j] = 0x1 << i;
+                    count++;
+                }
+            }
+        }
+    }
 }
--- lib/poker.h
+++ lib/poker.h
@@ -12,6 +12,18 @@ typedef struct {
     hand h;
 } handEvaluation;
 
-handEvaluation evaluate(hand*);
+unsigned char countBits(cardSet);
+
+// handEvaluation evaluate(hand*);
+
+// These three check functions change handEvaluation->handType
+// to 0 if they don't contain the respective hand type.
+// Otherwise they change handEvaluation->handType to the proper constant.
+// The provided handEvaluation must be fully zeroed for each of the check functions.
+void checkFlush(handEvaluation*, hand*);
+// void checkStraight(handEvaluation*, hand*);
+// void checkStraightFlush(handEvaluation*, hand*);
+
+// void checkSets(handEvaluation*, hand*);
 
 #endif // POKER_H
