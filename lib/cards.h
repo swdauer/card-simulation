8edead1a3888826311bdfac921eebdceb76234b9
Scott Dauer <swdauer@gmail.com>
2021-09-15 11:18:48 -0500

    Sort header by type

--- lib/cards.h
+++ lib/cards.h
@@ -1,38 +1,48 @@
 #ifndef CARDS_H
 #define CARDS_H
 
+/* card type */
 #define NUM_SUITS 4
 #define LG_NUM_SUITS 2 // this should be the log base 2 of NUM_SUITS
-#define DECK_SIZE 52
+#define NUM_RANKS 13
 
 // These macros only work if NUM_SUITS is a power of 2
 #define SUIT(c) ((c) & (NUM_SUITS - 1)) // get last two bits from c
 #define RANK(c) ((c) >> LG_NUM_SUITS) // get c / NUM_SUITS
 
-typedef char card;
-typedef card deck[DECK_SIZE];
-typedef struct hand {
-    card c;
-    struct hand* next;
-} hand;
-
-void initDeck(deck);
+typedef unsigned char card;
 
 char rankChar(card);
 char suitChar(card);
 
 void swap(card*, card*);
 
+void printCard(card);
+/* end card type */
+
+/* hand type */
+typedef struct hand {
+    card c;
+    struct hand* next;
+} hand;
+
+void printHand(hand*);
+
+hand* addToHand(hand* head, hand* allocSpace, card);
+/* end hand type */
+
+/* deck type */
+#define DECK_SIZE (NUM_SUITS*NUM_RANKS)
+
+typedef card deck[DECK_SIZE];
+
+void initDeck(deck);
+
 void shuffleMtoN(deck, char m, char n);
 void shuffle(deck);
 
-void printCard(card);
 void printMtoN(deck, char m, char n);
 void printDeck(deck);
-void printHand(hand*);
-
-// void quicksortMtoN(deck, char m, char n);
-
-hand* addToHand(hand* head, hand* allocSpace, card);
+/* end deck type */
 
 #endif // CARDS_H
