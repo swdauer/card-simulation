fe236f34881779a3071fd9ed51c1b78f31474209
Scott Dauer <swdauer@gmail.com>
2021-09-20 16:32:15 -0500

    Add dataWriter to collect data

---
+++ dataWriter.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "lib/cards.h"
+#include "lib/poker.h"
+
+#define NUM_TRIALS 100000
+
+// two holdem hands vs eachother
+void holdemHeadsUp(int numTrials, deck d) {
+    printf(
+        "hand1_0,hand1_1,h1_type,"
+        "hand2_0,hand2_1,h2_type,"
+        "board_0,board_1,board_2,"
+        "board_3,board_4,winner\n"
+    );
+    int i;
+    for (i = 0; i < numTrials; i++) {
+        shuffle(d);
+
+        hand board = {};
+        hand h1Hole = {};
+        hand h1 = {};
+        hand h2Hole = {};
+        hand h2 = {};
+
+        int j;
+        for (j = 0; j < 5; j++) {
+            addToHand(&board, d[j]);
+            addToHand(&h1, d[j]);
+            addToHand(&h2, d[j]);
+        }
+        for (j = 5; j < 7; j++) {
+            addToHand(&h1Hole, d[j]);
+            addToHand(&h1, d[j]);
+        }
+        for (j = 7; j < 9; j++) {
+            addToHand(&h2Hole, d[j]);
+            addToHand(&h2, d[j]);
+        }
+
+        handEvaluation e1 = evaluate(&h1);
+        handEvaluation e2 = evaluate(&h2);
+
+        int comp = compareHE(&e1, &e2);
+
+        printHand(&h1Hole);
+        putchar(',');
+        printHandEval(&e1);
+        putchar(',');
+        printHand(&h2Hole);
+        putchar(',');
+        printHandEval(&e2);
+        putchar(',');
+        printHand(&board);
+        putchar(',');
+        if (comp == 1) putchar('1');
+        else if (comp == -1) putchar('2');
+        else putchar('0');
+        putchar('\n');
+
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    deck d;
+    initDeck(d);
+    holdemHeadsUp(NUM_TRIALS, d);
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
+dataWriter.exe: dataWriter.c lib/cards.c lib/poker.c
+	gcc -o dataWriter.exe dataWriter.c lib/cards.c lib/poker.c -ggdb
+
 test.exe: test.c lib/cards.c lib/poker.c
 	gcc -o test.exe test.c lib/cards.c lib/poker.c -ggdb
